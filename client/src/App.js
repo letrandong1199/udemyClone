c56d2cb011aa43c04c4c0a3b4afa6d4695e39969
zzf7ktx <zzf7ktx@gmail.com>
2021-07-03 21:55:53 +0700

    Add titlebar and logo

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,108 @@
-import logo from './logo.svg';
+/* eslint-disable no-undef */
 import './App.css';
+import { fade, makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import InputBase from '@material-ui/core/InputBase';
+import SearchIcon from '@material-ui/icons/Search';
+import ButtonBase from '@material-ui/core/ButtonBase';
+import { ReactComponent as ReactLogo } from './logo.svg'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)'
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  catg: {
+    position: 'relative',
+    color: 'rgb(255, 255, 255)',
+    marginRight: '50px',
+    marginLeft: '50px'
+  },
+  search: {
+    position: 'relative',
+    flexGrow: 1,
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(3),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
+  logoButton: {
+    width: '200px',
+    height: '50px',
+    marginTop: '10px',
+    marginBottom: '10px'
+  },
+  logo: {
+    width: '100%',
+    height: '100%',
+    padding: '0',
+    margin: '0'
+  }
+}));
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppBar position="sticky" className={classes.root}>
+      <Toolbar>
+        <ButtonBase className={classes.logoButton}>
+          <ReactLogo className={classes.logo} />
+        </ButtonBase>
+        <div className={classes.search}>
+          <div className={classes.searchIcon}>
+            <SearchIcon />
+          </div>
+          <InputBase
+            placeholder="Search…"
+            classes={{
+              root: classes.inputRoot,
+              input: classes.inputInput,
+            }}
+            inputProps={{ 'aria-label': 'search' }}
+          />
+        </div>
+
+        <Button className={classes.catg}>Categories</Button>
+        <Button color="inherit">Login</Button>
+      </Toolbar>
+    </AppBar>
   );
 }
 
