249239cfe48b201dd7790aa44482d12cf6ef3dff
zzf7ktx <zzf7ktx@gmail.com>
2021-07-06 21:48:46 +0700

    Add custom css with animation and autoplay for carousel Add animation for react-animated-carousel component. And custom slider content: <h1>, <button>, <p>.
    Custom this component by Carousel.css.
    
    Now, having trasition and animation of inner content every time carousel has changing.

--- client/src/components/Carousel.js
+++ client/src/components/Carousel.js
@@ -1,6 +1,8 @@
 import Slider from 'react-animated-slider';
 import horizontalCss from 'react-animated-slider/build/horizontal.css';
 import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import carouselCss from './Carousel.css';
 
 const useStyles = makeStyles((theme) => ({
     center: {
@@ -14,8 +16,114 @@ const useStyles = makeStyles((theme) => ({
         //top: '50%',
         //left: '50%',
         //transform: 'translate(-50 %, -50 %)'
+    },
+    inner: {
+        padding: '0 70px',
+        boxSizing: 'border-box',
+        position: 'relative',
+        width: '100%',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+    },
+    slider: {
+        position: 'relative',
+        width: '100%',
+        height: '400px',
+        overflow: 'hidden',
+        '& a': {
+            '&.previousButton, &.nextButton': {
+                fontSize: '22px',
+                lineHeight: 0,
+                display: 'block',
+                position: 'absolute',
+                top: '50%',
+                transform: 'translateY(-50%)',
+                transition: 'all .3s linear',
+                zIndex: 1,
+                padding: '10px',
+                textDecoration: 'none',
+                backfaceVisibility: 'hidden',
+                '& svg': {
+                    '& polygon': {
+                        fill: '#ffd800',
+                    }
+                },
+                '&:not(.disabled):hover': {
+                    transform: 'translateY(-50 %) scale(1.25)',
+                    cursor: 'pointer',
+                }
+            },
+            '&.previousButton': {
+                left: '20px',
+            },
+            '&.nextButton': {
+                right: '20px',
+            }
+
+        }
+    },
+    sliderContent: {
+        width: '100%',
+        height: '100%',
+        position: 'absolute',
+        overflow: 'hidden',
+        textAlign: 'center',
+        backgroundSize: 'cover !important',
+        '&::before': {
+            content: '""',
+            display: 'block',
+            position: 'absolute',
+            width: '100%',
+            height: '100%',
+            background: 'linear-gradient(transparent, rgba(0, 0, 0, 0.9))',
+            bottom: 0,
+            left: 0,
+        },
+        '&.hidden': {
+            visibility: 'hidden',
+        },
+        '&.previous': {
+            left: '-100%',
+        },
+        '&.current': {
+            left: 0,
+        },
+        '&.next': {
+            left: '100%',
+        },
+        '&.animateIn, &.animateOut': {
+            webkitTransition: 'all 2s ease',
+            transition: 'all 2s ease',
+            '&.previous, &.next': {
+                left: 0,
+                visibility: 'visible',
+                '& .MuiTypography-subtitle1': {
+                    transitionDelay: '1.1s',
+                },
+                '& .MuiButton': {
+                    transitionDelay: '1.3s',
+                }
+
+
+            },
+            '&.previous': {
+                left: '100%',
+            },
+            '&.next': {
+                left: '-100%',
+            },
+            '& .MuiTypography-subtitle1': {
+                transitionDelay: '.3s',
+            },
+            '& .MuiButton': {
+                transitionDelay: '.2s',
+            }
+        },
+
     }
-}));
+}
+));
 
 const content = [
     {
@@ -42,14 +150,14 @@ const content = [
 function Carousel() {
     const classes = useStyles();
     return (
-        <Slider classNames={horizontalCss} >
+        <Slider classNames={carouselCss} autoplay={2000}>
             {content.map((item, index) => (
                 <div
                     key={index}
                     style={{ background: `url('${item.image}') no-repeat center center` }}
-                    className={classes.center}
+                    className={carouselCss.slide}
                 >
-                    <div className="dd">
+                    <div className={classes.inner}>
                         <h1>{item.title}</h1>
                         <p>{item.description}</p>
                         <button>{item.button}</button>
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -23,7 +23,7 @@ const useStyles = makeStyles((theme) => ({
         background: 'white', //'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
         boxShadow: '0 3px 5px 2px rgba(75, 75, 75, .3)', //'0 3px 5px 2px rgba(255, 105, 135, .3)'
         justifyContent: 'space-between',
-        position: 'fixed'
+        position: 'static'
 
     },
     menuButton: {
