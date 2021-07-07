d518e8cc86de12cb6fa23dc9ca23a70721a65d1a
Le Tran Dong <letrandong1199@gmail.com>
2021-07-07 20:49:56 +0700

    ADD: Table User & Role

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -2,10 +2,15 @@
 require("dotenv").config();
 module.exports = {
   development: {
-    client: "postgresql",
+    client: "pg",
     connection: {
-      connectionString: process.env.DB_URI,
-      ssl: { rejectUnauthorized: false },
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      ssl: false,
+      // connectionString: process.env.DB_URI,
+      // ssl: { rejectUnauthorized: false },
     },
     pool: {
       min: 2,
@@ -13,6 +18,9 @@ module.exports = {
     },
     migrations: {
       tableName: "knex_migrations",
+    },
+    seeds: {
+      directory: "./seeds/",
     },
   },
 };
