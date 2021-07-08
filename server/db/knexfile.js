29f271f50d5765fc875718c174678fb3d02bcc89
Le Tran Dong <letrandong1199@gmail.com>
2021-07-08 18:22:04 +0700

    fix config heroku

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -4,20 +4,38 @@ module.exports = {
   development: {
     client: "pg",
     connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: process.env.DB_NAME,
-      ssl: false,
-      // connectionString: process.env.DB_URI,
-      // ssl: { rejectUnauthorized: false },
+      host: "localhost",
+      database: "knex_tutorial",
+      user: "postgres",
+      password: "admin",
     },
     pool: {
       min: 2,
       max: 10,
     },
     migrations: {
-      tableName: "knex_migrations",
+      directory: "./migrations",
+    },
+    seeds: {
+      directory: "./seeds/",
+    },
+  },
+  production: {
+    client: "pg",
+    connection: {
+      host: process.env.DB_HOST,
+      port: 5432,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      url: process.env.DATABASE_URI,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      directory: "./migrations",
     },
     seeds: {
       directory: "./seeds/",
--- server/db/migrations/20210708110311_UdemyManagementSystem.js
+++ server/db/migrations/20210708110311_UdemyManagementSystem.js
@@ -1,20 +1,20 @@
 exports.up = function (knex) {
   return knex.schema
+    .createTable("Role", function (table) {
+      table.increments("Id").primary();
+      table.string("Name").notNullable();
+    })
     .createTable("Users", function (table) {
-      table.increments();
+      // table.increments();
       table.string("Email").notNullable().primary();
       table.string("Full_Name").notNullable();
       table.string("Password").notNullable();
       table.timestamp("created_at").defaultTo(knex.fn.now());
       table.timestamp("updated_at").defaultTo(knex.fn.now());
-      table.integer("Role_Id").references("Id").inTable("Role");
-    })
-    .createTable("Role", function (table) {
-      table.increments("Id").primary();
-      table.string("Name").notNullable();
+      table.integer("Role_Id").unsigned().references("Id").inTable("Role");
     });
 };
 
 exports.down = function (knex) {
-  return knex.schema.dropTable("Users").dropTable("Role");
+  return knex.schema.dropTable("Role").dropTable("Users");
 };
