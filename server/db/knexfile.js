739d05f026344e084fccdbbf5d480d02f1e79c34
zzf7ktx <41227962+zzf7ktx@users.noreply.github.com>
2021-07-09 18:09:33 +0700

    Update knexfile.js

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -1,20 +1,30 @@
 // Update with your config settings.
 require("dotenv").config();
 module.exports = {
-  production: {
-    client: "pg",
-    connection: process.env.DATABASE_URL,
-    
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/lessons.db3',
+    },
+    useNullAsDefault: true,
     pool: {
-      min: 2,
-      max: 10,
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    },
+  },
+  production: {
+    client: 'pg',
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
     },
     migrations: {
-      tableName: "knex_migrations",
-      directory: './migrations',
+      directory: __dirname + '/migrations',
     },
     seeds: {
-      directory: "./seeds/",
+      directory: __dirname + '/seeds',
     },
   },
 };
+
