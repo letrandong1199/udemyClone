e96e6259e71a0d2ade002e33d4d974e0210f7823
zzf7ktx <41227962+zzf7ktx@users.noreply.github.com>
2021-07-09 17:40:49 +0700

    Update knexfile.js

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -3,15 +3,15 @@ require("dotenv").config();
 module.exports = {
   development: {
     client: "pg",
-    connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: process.env.DB_NAME,
-      ssl: false,
+    connection: process.env.DB_URI,
+      //host: process.env.DB_HOST,
+      //user: process.env.DB_USER,
+      //password: process.env.DB_PASS,
+      //database: process.env.DB_NAME,
+      //ssl: false,
       // connectionString: process.env.DB_URI,
       // ssl: { rejectUnauthorized: false },
-    },
+    
     pool: {
       min: 2,
       max: 10,
