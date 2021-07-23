e134284bca2792c011ccb9f47d87a70cdc43e73c
zzf7ktx <zzf7ktx@gmail.com>
2021-07-23 16:26:06 +0700

    Fix chat bot server

--- chatbot/src/routes.js
+++ chatbot/src/routes.js
@@ -7,6 +7,82 @@ router.get('/', (req, res) => {
     res.send('Hello');
 });
 
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+    let response;
+
+    // Check if the message contains text
+    if (received_message.text) {
+
+        // Create the payload for a basic text message
+        response = {
+            "text": `You sent the message: "${received_message.text}". Now send me an image!`
+        }
+    }
+
+    // Sends the response message
+    callSendAPI(sender_psid, response);
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+    let response;
+
+    // Get the payload for the postback
+    let payload = received_postback.payload;
+
+    // Set the response based on the postback payload
+    if (payload === 'yes') {
+        response = { "text": "Thanks!" }
+    } else if (payload === 'no') {
+        response = { "text": "Oops, try sending another image." }
+    } else if (payload === 'Get Started') {
+        response = {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "button",
+                    "text": "What do you want to do next?",
+                    "buttons": [
+                        {
+                            "type": "message",
+                            "url": "https://www.messenger.com",
+                            "title": "Visit Messenger"
+                        }
+                    ]
+                }
+            }
+        }
+    }
+    // Send the message to acknowledge the postback
+    callSendAPI(sender_psid, response);
+
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+    // Construct the message body
+    let request_body = {
+        "recipient": {
+            "id": sender_psid
+        },
+        "message": response
+    }
+    // Send the HTTP request to the Messenger Platform
+    request({
+        "uri": "https://graph.facebook.com/v2.6/me/messages",
+        "qs": { "access_token": PAGE_ACCESS_TOKEN },
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+            console.log('message sent!')
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    });
+}
+
 router.post('/webhook', (req, res) => {
 
     let body = req.body;
