beb2fb81b3d83a7b967115ba530d95e6f2b31bf3
zzf7ktx <zzf7ktx@gmail.com>
2021-07-23 18:30:50 +0700

    Fix missing callApi function

--- chatbot/src/routes/chatbot.route.js
+++ chatbot/src/routes/chatbot.route.js
@@ -48,7 +48,29 @@ async function handlePostback(sender_psid, received_postback) {
     callSendAPI(sender_psid, response);
 }
 
-
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+    // Construct the message body
+    let request_body = {
+        "recipient": {
+            "id": sender_psid
+        },
+        "message": response
+    }
+    // Send the HTTP request to the Messenger Platform
+    request({
+        "uri": "https://graph.facebook.com/v2.6/me/messages",
+        "qs": { "access_token": PAGE_ACCESS_TOKEN },
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+            console.log('message sent!')
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    });
+}
 
 router.post('/webhook', (req, res) => {
 
