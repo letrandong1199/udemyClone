9b3732b32ca3d0616fe37e83ec8f68cf19cbf9ea
zzf7ktx <zzf7ktx@gmail.com>
2021-07-23 15:26:37 +0700

    Init chatbot app

---
+++ chatbot/src/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+const routes = require('./routes');
+
+app.use('/', routes);
+
+let port = process.env.PORT || 3033;
+app.listen(port, () => {
+    console.log(`listening on ${port}`);
+});
