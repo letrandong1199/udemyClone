40e508eae9613a40b703f2573721dc001d157241
Le Tran Dong <letrandong1199@gmail.com>
2021-07-30 17:28:39 +0700

    ADD: API search and sort Course

--- server/repositories/course.repository.js
+++ server/repositories/course.repository.js
@@ -1,22 +1,31 @@
 const db = require("../db/db");
 const operatorType = require("../utils/enums/operatorType");
+const categoryRepository = require("./category.repository");
 const courseRepository = {
-  getCourseByQuery(query, paging) {
-    console.log(query, paging);
-    return db("Courses").where(qb => {
-      for (const [key, value] of Object.entries(query)) {
-        qb.whereIn(key, value);
-      }
-
-    }).limit(paging.limit).offset(paging.offset)
+  getCourseByQuery(query, paging, search, sort) {
+    console.log(query, paging, search.category);
+    return db("Courses")
+      .where((qb) => {
+        for (const [key, value] of Object.entries(query)) {
+          qb.whereIn(key, value);
+        }
+      })
+      .where("Title", "like", `%${search.search}%`)
+      .orWhereIn("Category_Id", search.category)
+      .orderBy(sort.ColName, sort.OrderBy)
+      .limit(paging.limit)
+      .offset(paging.offset)
       .catch(() => operatorType.FAIL.NOT_EXIST);
   },
   getCountCourses(query) {
-    return db("Courses").where(qb => {
-      for (const [key, value] of Object.entries(query)) {
-        qb.whereIn(key, value);
-      }
-    }).count('Id', { as: 'Count' }).first()
+    return db("Courses")
+      .where((qb) => {
+        for (const [key, value] of Object.entries(query)) {
+          qb.whereIn(key, value);
+        }
+      })
+      .count("Id", { as: "Count" })
+      .first();
   },
 
   getCourseByTitle(title) {
