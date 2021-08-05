5ff64e5db3f50c6c5ed962e8bf79dbfef9170bdd
zzf7ktx <zzf7ktx@gmail.com>
2021-08-05 12:32:16 +0700

    Fix quick reply message response

--- chatbot/src/controllers/chatbot.controller.js
+++ chatbot/src/controllers/chatbot.controller.js
@@ -13,9 +13,6 @@ async function handleMessage(sender_psid, received_message) {
     if (received_message.text) {
         console.log(received_message)
         if (received_message.quick_reply && received_message.quick_reply.payload) {
-            if (message.quick_reply.payload !== " ") {
-                console.log(message.quick_reply.payload);
-            }
             const prefix = message.quick_reply.payload.split("-");
             if (message.quick_reply.prefix[0] === "CATEGORY") {
                 await chatbotService.handleGetCoursesByCategory(prefix[1]);
